2fb1914b2bf17d20d9addcc3d544d129a6688711
alaaseada <alaaseada87@gmail.com>
2022-08-14 08:43:50 +0200

    Add projects from 4 to 7

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,39 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [amount, setAmount] = useState(0);
+  const [content, setContent] = useState([]);
+
+  function handleSubmission(e) {
+    e.preventDefault();
+    let amount_val = 0;
+    amount_val = amount < 0 ? 1 : amount > 8 ? 8 : amount;
+    setContent(data.slice(0, amount_val));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring Lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmission}>
+        <label htmlFor="amount">Pharagraphs: </label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        ></input>
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {content.map((line) => {
+          return <p>{line}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
